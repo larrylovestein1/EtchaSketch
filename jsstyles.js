48205cdc5da41e0ebb1250bd66c4e0bc999d2eff
maxum <larrylovesteinburner@gmail.com>
2023-01-25 21:48:46 -0600

    im adding my grid boxes with js for formatting correctly / using best practices. I added my 1st grid as a test cause i was rusty on dom manipulation

---
+++ jsstyles.js
@@ -0,0 +1,7 @@
+const gridContainer = document.querySelector('#gridContainer');
+
+
+const g1 = document.createElement('div');
+g1.classList.add('g1');
+g1.textContent = 'this is a test'
+gridContainer.appendChild(g1);
