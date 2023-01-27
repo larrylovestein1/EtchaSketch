452ed32af08fed90f78d7d419c0cb8fbaaa54d66
maxum <larrylovesteinburner@gmail.com>
2023-01-26 21:44:14 -0600

    instead of individually writing out the div classes and forming into columns and rows 256 times... i just made a function to do that for me and used CSS grid styling to apply them.

--- jsstyles.js
+++ jsstyles.js
@@ -1,7 +1,14 @@
-const gridContainer = document.querySelector('#gridContainer');
+const gridContainer = document.getElementById("gridContainer");
 
+//function to make both rows and columns
+function makeRows(rows, cols) {
+  gridContainer.style.setProperty('--grid-rows' , rows);
+  gridContainer.style.setProperty('--grid-cols' , cols);
+  for (c = 0; c < (rows * cols); c++) {
+    let cell = document.createElement("div");
+    cell.innerText = (c + 1);
+    gridContainer.appendChild(cell).className = "grid-item";
+  };
+};
 
-const g1 = document.createElement('div');
-g1.classList.add('g1');
-g1.textContent = 'this is a test'
-gridContainer.appendChild(g1);
+makeRows(16, 16);
