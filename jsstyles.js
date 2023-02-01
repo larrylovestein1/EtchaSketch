cb348b243a4d724b26e48d7bf18885df3aa5c5fa
maxum <larrylovesteinburner@gmail.com>
2023-01-31 21:55:36 -0600

    I had to rework essentially the same function in my js to create the grid. I think a var was wrong in the loop iteration. I also changed the styling sheets and am happy with the current layout. Will compact and clean up next

--- jsstyles.js
+++ jsstyles.js
@@ -1,13 +1,13 @@
-const gridContainer = document.getElementById("gridContainer");
+const container = document.getElementById("container");
 
-//function to make both rows and columns
-function makeRows(rows, cols) {
-  gridContainer.style.setProperty('--grid-rows' , rows);
-  gridContainer.style.setProperty('--grid-cols' , cols);
-  for (c = 0; c < (rows * cols); c++) {
+//makes rows
+function makeRows(rows,cols) {
+  container.style.setProperty('--grid-rows', rows);
+  container.style.setProperty('--grid-cols', cols);
+  for (i = 0; i < (rows * cols); i++) {
     let cell = document.createElement("div");
-    cell.innerText = (c + 1);
-    gridContainer.appendChild(cell).className = "grid-item";
+    cell.innerText = (i + 1);
+    container.appendChild(cell).className = "grid-item";
   };
 };
 
