8cd616f24d78fc74fc72ee93d00d170e44a88b68
maxum <larrylovesteinburner@gmail.com>
2023-02-14 20:02:35 -0600

    I added a function / event listener inside of the function to create the grid. It listens and then cahnges the grid items div color. I also briefly adjusted the centering and postioning of everyting

--- jsstyles.js
+++ jsstyles.js
@@ -1,14 +1,28 @@
 const container = document.getElementById("container");
 
-//makes rows
+
+
+
+//makes grid
 function makeRows(rows,cols) {
   container.style.setProperty('--grid-rows', rows);
   container.style.setProperty('--grid-cols', cols);
   for (i = 0; i < (rows * cols); i++) {
     let cell = document.createElement("div");
+    cell.addEventListener('mousemove',changeColors);
+    function changeColors() {
+      cell.style.backgroundColor = 'blue';
+    }
     cell.innerText = (i + 1);
     container.appendChild(cell).className = "grid-item";
   };
 };
 
 makeRows(16, 16);
+
+
+
+
+
+
+
