25d13cb267801baf6a13aa78fd07acdd9dd20979
maxum <larrylovesteinburner@gmail.com>
2023-02-27 22:30:58 -0600

    okay so i added top the js and just used some nested functions to use a button to reset my grids color back to normal when the user is done

--- jsstyles.js
+++ jsstyles.js
@@ -1,5 +1,6 @@
 const container = document.getElementById("container");
 const reset = document.getElementById("reset");
+const cell = document.getElementsByName("grid-item");
 
 
 
@@ -9,10 +10,18 @@ function makeRows(rows,cols) {
   container.style.setProperty('--grid-cols', cols);
   for (i = 0; i < (rows * cols); i++) {
     let cell = document.createElement("div");
-    cell.addEventListener('mousedown',changeColors); 
+    cell.addEventListener('mouseover',changeColors); 
+    //created nested function to use the cell variable inside
     function changeColors() {
       cell.style.backgroundColor = 'blue';
-    }
+    };
+    //created another nested function to access the cell variable 
+    //bc i cant access it outside of this scope, it works okay??..
+    reset.addEventListener('click' , resetGrid);
+    function resetGrid() {
+      cell.style.backgroundColor = 'grey';
+      console.log('somehow, this is working , progamming is beautiful huh?');
+    };
     cell.innerText = (i + 1);
     container.appendChild(cell).className = "grid-item";
   };
@@ -20,8 +29,10 @@ function makeRows(rows,cols) {
 
 makeRows(16, 16);
 
- console.log("hello world");
 
+
+
+  
 
 
 
