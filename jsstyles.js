e2d0c12c3c6dd8f7bee81977de6e3d45b0a72f9c
maxum <larrylovesteinburner@gmail.com>
2023-03-09 22:16:59 -0600

    i changed my css as well , after changing some things on js the container wasnt target correctly as it was declared as a id but i needed to refernce as a class. I decided to rework it all because i wanted to clean up the look so i used flex boxx on my grid container

--- jsstyles.js
+++ jsstyles.js
@@ -1,44 +1,59 @@
-const container = document.getElementById("container");
-const reset = document.getElementById("reset");
-const cell = document.getElementsByName("grid-item");
-const sizeButton = document.getElementById("prompt");
+let container = document.querySelector(".container");
+//const reset = document.getElementById("reset");
+//const cell = document.getElementsByName("grid-item");
+const sizeButton = document.querySelector("button");
 
 
+sizeButton.addEventListener('click' , ()=>{
+  resetSize();
+});
+
+function resetSize(){
+  let number = prompt("Choose your grid size! (1-100)");
+  container.style.gridTemplateRows = `repeat(${number}, 1fr)`;
+        container.style.gridTemplateColumns = `repeat(${number}, 1fr)`;
+        makeRows(number); // call the createGrid function here and pass the number they entered as the argument. 
+}
 
 //makes grid
-function makeRows(rows,cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
-  for (i = 0; i < (rows * cols); i++) {
-    let cell = document.createElement("div");
-    cell.addEventListener('mouseover',changeColors); 
-    //created nested function to use the cell variable inside
-    function changeColors() {
-      cell.style.backgroundColor = 'blue';
-    };
-    //created another nested function to access the cell variable 
-    //bc i cant access it outside of this scope, it works okay??..
-    reset.addEventListener('click' , resetGrid);
-    function resetGrid() {
-      cell.style.backgroundColor = 'grey';
-      console.log('somehow, this is working , progamming is beautiful huh?');
-    };
-    cell.innerText = (i + 1);
-    container.appendChild(cell).className = "grid-item";
-  };
-};
+function makeRows(size) {
+    //    resetSize(); // get rid of this as this was causing it to show the prompt again each time
+    
+    
+        container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+        container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+        
+        for (let i = 0; i < size*size; i++) {
+            
+            let cell = document.createElement("div");
+            cell.style.backgroundColor = "black";
+            container.appendChild(cell);
+    
+            //change background color of a square on hover
+            cell.addEventListener('mouseover', e=>{
+                cell.style.backgroundColor = "white";
+            })
+    
+    
+            //function to reset the grid
+            function clearGrid(){
+                sizeButton.addEventListener('click', e=>{
+                    cell.style.backgroundColor = "black"
+               })
+            }
+            
+    
+            clearGrid();    
+        }
+    }
+    
+    makeRows(16);
 
-makeRows(); //default grid size 
 
 
 
-sizeButton.addEventListener('click' , newGrid);
-// gather user input to use inside of the function for a new gridsize
-function newGrid() {
-  let userInput = prompt("Choose Grid Size");
-  console.log(userInput);
-  makeRows(userInput,userInput);
-};
+
+
 
 
 
